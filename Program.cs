df4540da85adc60ee3674b91596e04380af96723
Jose <augustomaitajam@gmail.com>
2024-05-21 05:09:02 -0300

    TP6 Terminado

--- Program.cs
+++ Program.cs
@@ -1,225 +1,303 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Runtime.Intrinsics.X86;
 using System.Threading.Tasks.Dataflow;
-// Console.WriteLine("Hello, World!");
-// int a;
-// int b;
-// a=10;
-// b=a;
-// Console.WriteLine("valor de a:"+a);
-// Console.WriteLine("valor de b:"+b);
-// Console.WriteLine("Hello, World!");
-// int a;
-// int b;
-// a=10;
-// b=a;
-// Console.WriteLine("valor de a:"+a);
-// Console.WriteLine("valor de b:"+b);
-
-// Console.WriteLine("Ingresar el numero que desea invertir: ");
-// int i;
-// string suma = "";
-// int dig;
-// string s = Console.ReadLine();
-// if (int.TryParse(s, out i))
-// {
-//     if (i >= 0)
-//     {
-//         while (i>0)
-//         {
-//             dig = i % 10;
-//             suma = suma + Convert.ToString(dig);
-//             i = i/10;        
-//             }
-//             Console.WriteLine(suma);
-//     }
-// }
-
-    Console.WriteLine("------CALCULADORA------");
-    int opcions = 1;
-    int salir = 1;
-while (opcions == 1)
-{
-    Console.WriteLine("----MENU----");
-    Console.WriteLine("1- Sumar");
-    Console.WriteLine("2- Restar");
-    Console.WriteLine("3- Multiplicar");
-    Console.WriteLine("4- Dividir");
-    Console.WriteLine("5- Valor Absoluto");
-    Console.WriteLine("6- Cuadrado");
-    Console.WriteLine("7- Raiz");
-    Console.WriteLine("8- Seno");
-    Console.WriteLine("9- Coseno");
-    Console.WriteLine("10- Parte entera de un numero real");
-    Console.WriteLine("11- Maximo Y Minimo entre 2 numeros");
-    Console.WriteLine("12- Salir");
-    Console.WriteLine("INGRESAR OPCION: ");
-    int menu=Convert.ToInt32(Console.ReadLine());
-    switch (menu)
-    {
-        case 1:
-            Console.WriteLine("Ingresar Numeros: ");
-            Console.WriteLine("X: ");
-            float X = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Y: ");
-            float Y = Convert.ToInt32(Console.ReadLine());
-            float Sumas = X + Y;
-            Console.WriteLine("La Suma de "+ X +" + " + Y +" es :"+Sumas);
-        break;
-        case 2:
-            float resta;
-            Console.WriteLine("Ingresar Numeros: ");
-            Console.WriteLine("X: ");
-            X = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Y: ");
-            Y = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Desea Restar X-Y (1) o Y-X (2)?:");
-        int opcionResta = Convert.ToInt32(Console.ReadLine());
-        if (opcionResta ==1)
-        {
-            resta = X - Y;
-            Console.WriteLine("La resta de "+ X +" - " + Y + " es: "+ resta);
-        }else
-        {
-            resta = Y - X;
-            Console.WriteLine("La resta de "+ Y +" - " + X + " es: "+ resta);
-        }
-        break;
-        case 3:
-            Console.WriteLine("Ingresar Numeros: ");
-            Console.WriteLine("X: ");
-            X = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Y: ");
-            Y = Convert.ToInt32(Console.ReadLine());
-            float Multiplicar = X * Y;
-            Console.WriteLine("El Producto de "+ X +" * " + Y +" es :"+ Multiplicar);
-        break;
-        case 4:
-        float dividir;
-        Console.WriteLine("Ingresar Numeros: ");
-        Console.WriteLine("X: ");
-        X = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Y: ");
-        Y = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Desea hacer X/Y (1) o Y/X (2)?:");
-        int opcionDividir = Convert.ToInt32(Console.ReadLine());
-        if (opcionDividir ==1)
-        {
-            if (Y != 0)
-            {
-            dividir = X / Y;
-            Console.WriteLine("La divicion de "+ X +" / " + Y + " es: "+ dividir);   
-            }
-            else{
-            Console.WriteLine("La divicion en 0 es Imposible");
-            }
-        }else
-        {
-            if (X != 0)
-            {
-            dividir = Y / X;
-            Console.WriteLine("La divicion de "+ Y +" / " + X + " es: "+ dividir);   
-            }
-            else{
-            Console.WriteLine("La divicion en 0 es Imposible");
-            }
-        }
-        break;
-        case 5:
-            Console.WriteLine("Ingresar Numero: ");
-            if (float.TryParse(Console.ReadLine(),out X))
-            {
-                float Xabs = Math.Abs(X); 
-                Console.WriteLine("El valor absoluto de "+ X +" es :"+ Xabs);  
-            }else
-            {
-                Console.WriteLine("Lo ingresado no es una Entrada Valida");
-            }
-        break;
-        case 6:
-            Console.WriteLine("Ingresar Numero: ");
-            if (float.TryParse(Console.ReadLine(),out X))
-            {
-                double Xcuad = Math.Pow(X,2); 
-                Console.WriteLine("El Cuadrado de "+ X +" es : "+ Xcuad);  
-            }else
-            {
-                Console.WriteLine("Lo ingresado no es una Entrada Valida");
-            }
-        break;
-        case 7:
-            Console.WriteLine("Ingresar Numero: ");
-            if (float.TryParse(Console.ReadLine(),out X))
-            {
-                double Xraiz = Math.Sqrt(X); 
-                Console.WriteLine("La raiz Cuadrada de "+ X +" es : "+ Xraiz);  
-            }else
-            {
-                Console.WriteLine("Lo ingresado no es una Entrada Valida");
-            }
-        break;
-        case 8:
-            Console.WriteLine("Ingresar Numero: ");
-            if (float.TryParse(Console.ReadLine(),out X))
-            {
-                double Xsen = Math.Sin(X); 
-                Console.WriteLine("El Seno de "+ X +" es : "+ Xsen);  
-            }else
-            {
-                Console.WriteLine("Lo ingresado no es una Entrada Valida");
-            }
-        break;
-        case 9:
-            Console.WriteLine("Ingresar Numero: ");
-            if (float.TryParse(Console.ReadLine(),out X))
-            {
-                double Xcos = Math.Cos(X); 
-                Console.WriteLine("El Coseno de "+ X +" es : "+ Xcos);  
-            }else
-            {
-                Console.WriteLine("Lo ingresado no es una Entrada Valida");
-            }
-        break;
-        case 10:
-            Console.WriteLine("Ingresar Numero: ");
-            if (float.TryParse(Console.ReadLine(),out X))
-            {
-                int Xent = Convert.ToInt32(X);
-                Console.WriteLine("El Seno de "+ X +" es : "+ Xent);  
-            }else{
-                Console.WriteLine("Lo ingresado no es una Entrada Valida");
-            }
-        break;
-        case 11:
-            float mayor = 0;
-            Console.WriteLine("Ingresar Primer Numero: ");
-            if (float.TryParse(Console.ReadLine(),out X))
-            {
-                Console.WriteLine("Ingresar segundo Numero: ");
-                if(float.TryParse(Console.ReadLine(),out Y)){
-                    if (X > Y){
-                        mayor = X;
-                        Console.WriteLine("El numero mayor entre ("+X+","+Y+") es: "+ mayor);
-                    }else{
-                        mayor = Y;
-                        Console.WriteLine("El numero mayor entre ("+X+","+Y+") es: "+ mayor);
-                    }
-                }else
-                {
-                    Console.WriteLine("Lo ingresado no es una Entrada Valida");
-                }
-            }else{
-                Console.WriteLine("Lo ingresado no es una Entrada Valida");
-            }
-        break;
-        case 12:
-        salir = 0;
-        opcions = 0;
-        break;
+using Microsoft.VisualBasic;
+holaMundo();
+revertir();
+calculadora();
+cadenaDeTexto();
+static void holaMundo(){
+    Console.WriteLine("Hello, World!");
+ int a;
+ int b;
+ a=10;
+ b=a;
+ Console.WriteLine("valor de a:"+a);
+ Console.WriteLine("valor de b:"+b);
+}
+static void revertir(){
+     Console.WriteLine("Ingresar el numero que desea invertir: ");
+ int i;
+ string suma = "";
+ int dig;
+ string s = Console.ReadLine();
+ if (int.TryParse(s, out i))
+ {
+     if (i >= 0)
+     {
+         while (i>0)
+         {
+             dig = i % 10;
+             suma = suma + Convert.ToString(dig);
+             i = i/10;        
+             }
+             Console.WriteLine(suma);
     }
-    if (salir == 1)
+}
+}
+ static void calculadora(){
+     Console.WriteLine("------CALCULADORA------");
+     int opcions = 1;
+     int salir = 1;
+ while (opcions == 1)
+ {
+     Console.WriteLine("----MENU----");
+     Console.WriteLine("1- Sumar");
+     Console.WriteLine("2- Restar");
+     Console.WriteLine("3- Multiplicar");
+     Console.WriteLine("4- Dividir");
+     Console.WriteLine("5- Valor Absoluto");
+     Console.WriteLine("6- Cuadrado");
+     Console.WriteLine("7- Raiz");
+     Console.WriteLine("8- Seno");
+     Console.WriteLine("9- Coseno");
+     Console.WriteLine("10- Parte entera de un numero real");
+     Console.WriteLine("11- Maximo Y Minimo entre 2 numeros");
+     Console.WriteLine("12- Salir");
+     Console.WriteLine("INGRESAR OPCION: ");
+     int menu=Convert.ToInt32(Console.ReadLine());
+     switch (menu)
+     {
+         case 1:
+             Console.WriteLine("Ingresar Numeros: ");
+             Console.WriteLine("X: ");
+             float X = Convert.ToInt32(Console.ReadLine());
+             Console.WriteLine("Y: ");
+             float Y = Convert.ToInt32(Console.ReadLine());
+             float Sumas = X + Y;
+             Console.WriteLine("La Suma de "+ X.ToString() +" + " + Y.ToString() +" es :" + Sumas.ToString());
+         break;
+         case 2:
+             float resta;
+             Console.WriteLine("Ingresar Numeros: ");
+             Console.WriteLine("X: ");
+             X = Convert.ToInt32(Console.ReadLine());
+             Console.WriteLine("Y: ");
+             Y = Convert.ToInt32(Console.ReadLine());
+             Console.WriteLine("Desea Restar X-Y (1) o Y-X (2)?:");
+         int opcionResta = Convert.ToInt32(Console.ReadLine());
+         if (opcionResta ==1)
+         {
+             resta = X - Y;
+             Console.WriteLine("La resta de "+ X.ToString() +" - " + Y.ToString() + " es: "+ resta.ToString());
+         }else
+         {
+             resta = Y - X;
+             Console.WriteLine("La resta de "+ Y.ToString() +" - " + X.ToString() + " es: "+ resta.ToString());
+         }
+         break;
+         case 3:
+             Console.WriteLine("Ingresar Numeros: ");
+             Console.WriteLine("X: ");
+             X = Convert.ToInt32(Console.ReadLine());
+             Console.WriteLine("Y: ");
+             Y = Convert.ToInt32(Console.ReadLine());
+             float Multiplicar = X * Y;
+             Console.WriteLine("El Producto de "+ X.ToString() +" * " + Y.ToString() +" es :"+ Multiplicar.ToString());
+         break;
+         case 4:
+         float dividir;
+         Console.WriteLine("Ingresar Numeros: ");
+         Console.WriteLine("X: ");
+         X = Convert.ToInt32(Console.ReadLine());
+         Console.WriteLine("Y: ");
+         Y = Convert.ToInt32(Console.ReadLine());
+         Console.WriteLine("Desea hacer X/Y (1) o Y/X (2)?:");
+         int opcionDividir = Convert.ToInt32(Console.ReadLine());
+         if (opcionDividir ==1)
+         {
+             if (Y != 0)
+             {
+             dividir = X / Y;
+             Console.WriteLine("La divicion de "+ Convert.ToString(X) +" / " + Convert.ToString(Y) + " es: "+ Convert.ToString(dividir));   
+             }
+             else{
+             Console.WriteLine("La divicion en 0 es Imposible");
+             }
+         }else
+         {
+             if (X != 0)
+             {
+             dividir = Y / X;
+             Console.WriteLine("La divicion de "+ Y.ToString() +" / " + X.ToString() + " es: "+ dividir.ToString());   
+             }
+             else{
+             Console.WriteLine("La divicion en 0 es Imposible");
+             }
+         }
+         break;
+         case 5:
+             Console.WriteLine("Ingresar Numero: ");
+             if (float.TryParse(Console.ReadLine(),out X))
+             {
+                 float Xabs = Math.Abs(X); 
+                 Console.WriteLine("El valor absoluto de "+ X.ToString() +" es :"+ Xabs.ToString());  
+             }else
+             {
+                 Console.WriteLine("Lo ingresado no es una Entrada Valida");
+             }
+         break;
+         case 6:
+             Console.WriteLine("Ingresar Numero: ");
+             if (float.TryParse(Console.ReadLine(),out X))
+             {
+                 double Xcuad = Math.Pow(X,2); 
+                 Console.WriteLine("El Cuadrado de "+ X.ToString() +" es : "+ Xcuad.ToString());  
+             }else
+             {
+                 Console.WriteLine("Lo ingresado no es una Entrada Valida");
+             }
+         break;
+         case 7:
+             Console.WriteLine("Ingresar Numero: ");
+             if (float.TryParse(Console.ReadLine(),out X))
+             {
+                 double Xraiz = Math.Sqrt(X); 
+                 Console.WriteLine("La raiz Cuadrada de "+ X.ToString() +" es : "+ Xraiz.ToString());  
+             }else
+             {
+                 Console.WriteLine("Lo ingresado no es una Entrada Valida");
+             }
+         break;
+         case 8:
+             Console.WriteLine("Ingresar Numero: ");
+             if (float.TryParse(Console.ReadLine(),out X))
+             {
+                 double Xsen = Math.Sin(X); 
+                 Console.WriteLine("El Seno de "+ X.ToString() +" es : "+ Xsen.ToString());  
+             }else
+             {
+                 Console.WriteLine("Lo ingresado no es una Entrada Valida");
+             }
+         break;
+         case 9:
+             Console.WriteLine("Ingresar Numero: ");
+             if (float.TryParse(Console.ReadLine(),out X))
+             {
+                 double Xcos = Math.Cos(X); 
+                 Console.WriteLine("El Coseno de "+ X.ToString() +" es : "+ Xcos.ToString());  
+             }else
+             {
+                 Console.WriteLine("Lo ingresado no es una Entrada Valida");
+             }
+         break;
+         case 10:
+             Console.WriteLine("Ingresar Numero: ");
+             if (float.TryParse(Console.ReadLine(),out X))
+             {
+                 int Xent = Convert.ToInt32(X);
+                 Console.WriteLine("El Seno de "+ X.ToString() +" es : "+ Xent.ToString());  
+             }else{
+                 Console.WriteLine("Lo ingresado no es una Entrada Valida");
+             }
+         break;
+         case 11:
+             float mayor = 0;
+             Console.WriteLine("Ingresar Primer Numero: ");
+             if (float.TryParse(Console.ReadLine(),out X))
+             {
+                 Console.WriteLine("Ingresar segundo Numero: ");
+                 if(float.TryParse(Console.ReadLine(),out Y)){
+                     if (X > Y){
+                         mayor = X;
+                         Console.WriteLine("El numero mayor entre ("+X.ToString()+","+Y.ToString()+") es: "+ mayor.ToString());
+                     }else{
+                         mayor = Y;
+                         Console.WriteLine("El numero mayor entre ("+X.ToString()+","+Y.ToString()+") es: "+ mayor.ToString());
+                     }
+                 }else
+                 {
+                     Console.WriteLine("Lo ingresado no es una Entrada Valida");
+                 }
+             }else{
+                 Console.WriteLine("Lo ingresado no es una Entrada Valida");
+             }
+         break;
+         case 12:
+         salir = 0;
+         opcions = 0;
+         break;
+     }
+     if (salir == 1)
+     {
+     Console.WriteLine("Desea hacer otra operacion? (1: Si / 0 : NO)");
+     opcions = Convert.ToInt32(Console.ReadLine());
+     }
+ }
+ }
+static void cadenaDeTexto(){
+    Console.WriteLine("Ingresar Una Frase:");
+    string cadena = Console.ReadLine();
+    int longCad = Convert.ToInt32(Strings.Len(cadena));
+    Console.WriteLine($"La longitud de la Cadena es : {longCad}");
+    Console.WriteLine("Ingresar Una Segunda Frase:");
+    string cadena2 = Console.ReadLine();
+    string concat = String.Concat(cadena,cadena2);
+    Console.WriteLine($"La palabra Concatenada es: {concat}");
+    string subCad = concat.Substring(3,5);
+    Console.WriteLine($"La subCadena es : {subCad}");
+    calculadora();
+    foreach (char item in concat)
     {
-    Console.WriteLine("Desea hacer otra operacion? (1: Si / 0 : NO)");
-    opcions = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(item);
+    }
+    int index = 0;
+    int count = 0;
+    Console.WriteLine("Ingresar Una Palabra a Buscar en la frase Principal:");
+    string palabra = Console.ReadLine();
+            while ((index = cadena.IndexOf(palabra, index, StringComparison.OrdinalIgnoreCase)) != -1)
+        {
+            count++;
+            Console.WriteLine($"Ocurrencia encontrada en el índice: {index}");
+            index += palabra.Length; // Avanzar el índice para seguir buscando
+        }
+
+        if (count == 0)
+        {
+            Console.WriteLine("La palabra no fue encontrada en la cadena.");
+        }
+        else
+        {
+            Console.WriteLine($"La palabra '{palabra}' aparece {count} veces en la cadena.");
+        }
+    cadena = cadena.ToUpper();
+    Console.WriteLine($"La Cadena Principal en mayuscula es : {cadena}");
+    cadena = cadena.ToLower();
+    Console.WriteLine($"La Cadena Principal en mayuscula es : {cadena}");
+    string[] cadenaprueba = "ea,ae,auch".Split(',');
+    foreach (string item in cadenaprueba)
+    {
+        Console.WriteLine(item);
+    }
+    float pruebaString = 0;
+    Console.WriteLine("Ingrese una ecuacion simple (x+y, x-y, x*y, x/y): ");
+    string ecuacion = Console.ReadLine();
+    string[] ecuaciones;
+    if (ecuacion.IndexOf('+') > 0)
+    {
+        ecuaciones = ecuacion.Split('+');
+        foreach (string item in ecuaciones)
+        {
+            pruebaString = pruebaString + Convert.ToInt32(item);
+            
+        }
+        Console.WriteLine(pruebaString);
+    }else if (ecuacion.IndexOf('-') > 0)
+    {
+        ecuaciones = ecuacion.Split('-');
+        foreach (string item in ecuaciones)
+        {
+            pruebaString = pruebaString - Convert.ToInt32(item);
+        }
+        Console.WriteLine(pruebaString);
+    }else if (ecuacion.IndexOf('*') > 0)
+    {
+        ecuaciones = ecuacion.Split('*');
+        float pruebaString2 = Convert.ToInt32(ecuaciones[0]) * Convert.ToInt32(ecuaciones[1]);
+        Console.WriteLine(pruebaString2);
+    }else if (ecuacion.IndexOf('/') > 0){
+        ecuaciones = ecuacion.Split('/');
+        float pruebaString2 = Convert.ToInt32(ecuaciones[0]) / Convert.ToInt32(ecuaciones[1]);
+        Console.WriteLine(pruebaString2);
     }
 }
---
+++ readme.md
@@ -0,0 +1,113 @@
+# 1. ¿String es un tipo por valor o un tipo por referencia?
+En C#, string es un tipo por referencia. Aunque string tiene un comportamiento que parece de valor (porque es inmutable), internamente es un tipo de referencia. Esto significa que cuando asignas una variable de tipo string a otra, ambas variables apuntan a la misma instancia en la memoria. Si una variable cambia el contenido de la cadena, en realidad se crea una nueva cadena en la memoria, y la variable apunta a esta nueva cadena, dejando la original inalterada.
+
+# 2. ¿Qué secuencias de escape tiene el tipo string?
+Las secuencias de escape en un string en C# permiten incluir caracteres especiales que de otro modo serían difíciles de insertar directamente. Lista de las secuencias de escape más comunes:
+
+\\ - Barra invertida
+\' - Comilla simple
+\" - Comilla doble
+\0 - Carácter nulo
+\a - Alerta (beep)
+\b - Retroceso
+\f - Avance de página
+\n - Nueva línea
+\r - Retorno de carro
+\t - Tabulación horizontal
+\v - Tab
+### ulación vertical
+
+\uXXXX - Carácter Unicode (donde XXXX son cuatro dígitos hexadecimales)
+\xXX - Carácter Unicode (donde XX son uno o más dígitos hexadecimales)
+
+# 3. ¿Qué sucede cuando utiliza el carácter @ y $ antes de una cadena de texto?
+En C#, los caracteres @ y $ se utilizan para modificar el comportamiento de las cadenas de texto de las siguientes maneras:
+
+## Carácter @
+El carácter @ antes de una cadena de texto convierte la cadena en un string literal verbatim. Esto significa que la cadena se interpreta tal como está, ignorando las secuencias de escape. Además, permite que las cadenas se extiendan en varias líneas y se utilicen comillas dobles dobles "" para insertar una comilla doble dentro de la cadena.
+
+                                    string path = @"C:\Users\Nombre\Documentos";
+                                    string multiLine = @"Esta es una línea
+                                    y esta es otra línea.";
+                                    string quote = @"Él dijo: ""Hola Mundo!""";
+
+## Carácter $
+El carácter $ antes de una cadena de texto convierte la cadena en una cadena interpolada, lo que permite insertar expresiones dentro de la cadena que serán evaluadas y sus resultados serán incluidos en la cadena.
+
+                                    int a = 5;
+                                    int b = 10;
+                                    string interpolated = $"El valor de a es {a} y el valor de b es {b}.";
+
+## Combinación de @ y $
+Se puede combinar @ y $ para crear un string interpolado verbatim. Esto combina las ventajas de ambos modificadores, permitiendo cadenas de varias líneas con interpolación de valores.
+
+                                    string nombre = "Juan";
+                                    string multilineInterpolated = @$"Hola {nombre},
+                                    Esta es una cadena de varias líneas
+                                    con interpolación de valores.";
+
+# Conversor de tipo
+### ToString(): Convierte el objeto a su representación en cadena.
+
+                                    int number = 123;
+                                    string numberString = number.ToString(); // "123"
+# Comparador de cadenas
+### Compare(): Compara dos cadenas y devuelve un número entero que indica su relación de orden (negativo si la primera cadena es menor, positivo si es mayor, y cero si son iguales).
+
+                                    int result = string.Compare("abc", "def"); // -1
+                                    int result = string.Compare("abc", "abc"); // 0
+### CompareTo(): Compara la instancia actual con otra cadena y devuelve un número entero que indica su relación de orden.
+
+                                    string str1 = "abc";
+                                    string str2 = "def";
+                                    int result = str1.CompareTo(str2); // -1
+
+                                    == y !=: Operadores de igualdad y desigualdad que comparan dos cadenas.
+
+                                    bool areEqual = "abc" == "abc"; // true
+                                    bool areNotEqual = "abc" != "def"; // true
+# Mayúsculas y minúsculas
+### ToUpper(): Convierte la cadena a mayúsculas.
+
+                                    string upper = "hello".ToUpper(); // "HELLO"
+### ToLower(): Convierte la cadena a minúsculas.
+
+                                    string lower = "HELLO".ToLower(); // "hello"
+
+# Acceso a los caracteres individuales
+### SubString(): Extrae una subcadena de una cadena dada una posición inicial y, opcionalmente, una longitud.
+
+                                    string sub = "hello".Substring(1, 3); // "ell"
+### Replace(): Reemplaza todas las ocurrencias de un carácter o cadena por otro carácter o cadena.
+
+                                    string replaced = "hello".Replace('l', 'p'); // "heppo"
+### Split(): Divide una cadena en un arreglo de subcadenas utilizando uno o más delimitadores.
+
+                                    string[] parts = "a,b,c".Split(','); // ["a", "b", "c"]
+### Trim(): Elimina los caracteres de espacio en blanco al principio y al final de la cadena.
+
+                                    string trimmed = "  hello  ".Trim(); // "hello"
+# Búsqueda y manipulación de una cadena
+### IndexOf(): Devuelve el índice de la primera aparición de un carácter o cadena dentro de otra cadena.
+
+                                    int index = "hello".IndexOf('e'); // 1
+### LastIndexOf(): Devuelve el índice de la última aparición de un carácter o cadena dentro de otra cadena.
+
+                                    int lastIndex = "hello".LastIndexOf('l'); // 3
+### StartsWith(): Determina si una cadena comienza con una subcadena específica.
+
+                                    bool starts = "hello".StartsWith("he"); // true
+### EndsWith(): Determina si una cadena termina con una subcadena específica.
+
+                                    bool ends = "hello".EndsWith("lo"); // true
+
+# Funcionamiento de las expresiones regulares:
+- Las expresiones regulares permiten realizar búsquedas y manipulaciones de texto avanzadas, como encontrar patrones específicos, validar formatos de entrada, extraer información de texto, reemplazar partes de un texto, etc.
+- Funcionan mediante la definición de un patrón de búsqueda que consiste en caracteres literales y metacaracteres que representan clases de caracteres, cuantificadores, anclas, etc.
+- Se aplican sobre una cadena de texto y se evalúan de izquierda a derecha para encontrar coincidencias con el patrón especificado.
+# Aplicación de expresiones regulares:
+- Validación de Entrada: Puedes usar expresiones regulares para validar formatos de entrada, como direcciones de correo electrónico, números de teléfono, códigos postales, etc.
+- Extracción de Información: Son útiles para extraer información específica de cadenas de texto complejas, como números, fechas, nombres, etc.
+- Manipulación de Texto: Las expresiones regulares te permiten realizar búsquedas y reemplazos avanzados en el texto, como eliminar espacios en blanco, cambiar el formato de fechas, normalizar datos, etc.
+# Uso en C#:
+Las expresiones regulares no son exclusivas de C#; son una característica estándar que está disponible en muchos lenguajes de programación y herramientas de manipulación de texto. En C#, puedes hacer uso de las expresiones regulares a través del espacio de nombres System.Text.RegularExpressions.
